d530ea2c1266d8a8d8f028b3200f29f2271334cb
Hunter Flick <hflick@nd.edu>
2026-02-11 19:55:45 -0500

    matrix functionality moved to appropriate header and source files

---
+++ Matrix.hpp
@@ -0,0 +1,25 @@
+#ifndef __VEC_HPP__
+#define __VEC_HPP__
+
+#include <vec3.hpp>
+#include <iostream>
+#include <string>
+#define MATRIX_MAX 3
+
+class Matrix {
+    private:
+        float components[MATRIX_MAX][MATRIX_MAX];
+        vec3& operator[](int);
+    
+    public:
+        Matrix();    
+        Matrix(float[MATRIX_MAX][MATRIX_MAX]);
+        Matrix(const Matrix&);
+        Matrix& operator=(const Matrix&);
+        bool operator==(Matrix);
+        bool operator!=(Matrix);
+        Matrix operator*(Matrix);
+        Matrix transpose();
+};
+
+#endif
