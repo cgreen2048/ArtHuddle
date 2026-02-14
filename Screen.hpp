65bc11aa198cfd837a4a5b56b2cc75d640306edf
Clair Green <bgreen8@student11.cse.nd.edu>
2026-02-14 16:23:35 -0500

    Initialized Screen.hpp with member data & function prototypes, starting on Screen.cpp

---
+++ Screen.hpp
@@ -0,0 +1,26 @@
+#ifndef __SCREEN_HPP__
+#define __SCREEN_HPP__
+
+#include <iostream>
+#include <SDL3/SDL.h>
+#include "vec2.h"
+#include "vec3.h"
+
+class Screen {
+    private:
+        uint32_t width, height;
+        SDL_Surface* surface = nullptr;
+    public:
+        Screen();
+        Screen(uint32_t, uint32_t);
+        Screen(const Screen&);
+        ~Screen();
+        Screen& operator=(const Screen&);
+        void colorOnePixel(vec2, vec3);
+        void blitTo(SDL_Surface*);
+        void drawBresenhamLine();
+        void drawBox(ivec2, ivec2);
+        void drawBox(ivec3, ivec3);
+};
+
+#endif
