4444835083051812175a0b621bb81f932676bef3
Hunter Flick <hflick@nd.edu>
2026-02-17 12:55:40 -0500

    debugging, fixed so that Screen now compiles with sdl3

--- Screen.cpp
+++ Screen.cpp
@@ -5,52 +5,57 @@ Screen::Screen() : width{0}, height{0} {
 }
 
 Screen::Screen(uint32_t w, uint32_t h) : Screen() {
-    width = w;
-    height = h;
-    surface = SDL_CreateSurface(w, h, SDL_PIXELFORMAT_RGBA32);
+    this->width = w;
+    this->height = h;
+    this->surface = SDL_CreateSurface(w, h, SDL_PIXELFORMAT_RGBA32);
 }
 
 Screen::Screen(const Screen& cp) : Screen() {
-    width = cp.width;
-    height = cp.height;
-    surface = SDL_CreateSurface(width, height, SDL_PIXELFORMAT_RGBA32);
+    this->width = cp.width;
+    this->height = cp.height;
+    this->surface = SDL_CreateSurface(this->width, this->height, SDL_PIXELFORMAT_RGBA32);
     
     // must copy over the exact pixels from cp's surface to this surface
-    if (surface) {
-        cp.blitTo(surface);
+    if (this->surface) {
+        cp.blitTo(this->surface);
     }
 }
 
 Screen::~Screen() {
-    if (surface != nullptr) {
-        SDL_DestroySurface(surface);
+    if (this->surface != nullptr) {
+        SDL_DestroySurface(this->surface);
     }
 }
 
 
 Screen& Screen::operator=(const Screen& cp) {
-    if (*this == cp) {
+    // if (*this == *cp) {
+    //     return *this;
+    // }
+    if (this->surface->format == cp.surface->format) {  // need to test, format just states that it is the format of the surface
         return *this;
     }
 
-    width = cp.width;
-    height = cp.height;
-    surface = SDL_CreateSurface(width, height, SDL_PIXELFORMAT_RGBA32);
+    this->width = cp.width;
+    this->height = cp.height;
+    this->surface = SDL_CreateSurface(this->width, this->height, SDL_PIXELFORMAT_RGBA32);
     
-    if (surface) {
-        cp.blitTo(surface);
+    if (this->surface) {
+        cp.blitTo(this->surface);
     }
+    
+    return *this;
 }
 
-void Screen::blitTo(SDL_Surface* target){
-    if (target == nullptr){
+void Screen::blitTo(SDL_Surface* target) const {
+    if (target == nullptr) {
         std::cerr << "Target surface is a nullptr.\n";
         return;
     }
-    SDL_BlitSurface(surface, nullptr, target, nullptr); 
+    SDL_BlitSurface(this->surface, nullptr, target, nullptr); 
 }
 
-void Screen::drawBresenhamLine(ivec2 start, ivec2 end, ivec3 color){
+void Screen::drawBresenhamLine(ivec2 start, ivec2 end, ivec3 color) {
     int x0 = start.x;
     int y0 = start.y;
     int x1 = end.x;
@@ -65,7 +70,7 @@ void Screen::drawBresenhamLine(ivec2 start, ivec2 end, ivec3 color){
     int error = dx + dy;
 
     
-    while (true){
+    while (true) {
         colorOnePixel(ivec2(x0, y0),color);
 
         if (x0 == x1 && y0 == y1){
--- Screen.hpp
+++ Screen.hpp
@@ -18,7 +18,7 @@ class Screen {
         Screen(const Screen&);
         ~Screen();
         Screen& operator=(const Screen&);
-        void blitTo(SDL_Surface*);
+        void blitTo(SDL_Surface*) const;
         void drawBresenhamLine(ivec2, ivec2, ivec3);
 
         template<typename T1, typename T2>
@@ -28,8 +28,10 @@ class Screen {
                 + static_cast<int>(coords.x) * sizeof(uint32_t) 
                 + static_cast<int>(coords.y) * surface->pitch;
             uint32_t* pixel32 = reinterpret_cast<uint32_t*>(pixel);
+            const SDL_PixelFormatDetails *details =  SDL_GetPixelFormatDetails(surface->format);
             uint32_t pixelColor = SDL_MapRGBA(
-                surface->format, 
+                details,
+                nullptr, 
                 std::clamp(static_cast<int>(colors.x), 0, MAX_COLOR_VALUE), 
                 std::clamp(static_cast<int>(colors.y), 0, MAX_COLOR_VALUE), 
                 std::clamp(static_cast<int>(colors.z), 0, MAX_COLOR_VALUE), 
@@ -41,10 +43,10 @@ class Screen {
 
         template<typename T1, typename T2, typename T3>
         void drawBox(Tvec2<T1> min, Tvec2<T2> max, Tvec3<T3> colors) {
-            int minX = std::clamp(static_cast<int>(std::min(min.x, max.x)), 0, width-1);
-            int maxX = std::clamp(static_cast<int>(std::max(min.x, max.x)), 0, width-1);
-            int minY = std::clamp(static_cast<int>(std::min(min.y, max.y)), 0, height-1);
-            int maxY = std::clamp(static_cast<int>(std::max(min.y, max.y)), 0, height-1);
+            int minX = std::clamp(static_cast<int>(std::min(min.x, max.x)), 0, static_cast<int>(width-1));
+            int maxX = std::clamp(static_cast<int>(std::max(min.x, max.x)), 0, static_cast<int>(width-1));
+            int minY = std::clamp(static_cast<int>(std::min(min.y, max.y)), 0, static_cast<int>(height-1));
+            int maxY = std::clamp(static_cast<int>(std::max(min.y, max.y)), 0, static_cast<int>(height-1));
 
             for (int i = minX; i <= maxX; ++i) {
                 for (int j = minY; j <= maxY; ++j) {
