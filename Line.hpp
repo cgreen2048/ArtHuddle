42e2b348074ab6b975fda63de6f2110ecbc6f980
Freemankn <157552737+Freemankn@users.noreply.github.com>
2026-03-05 03:18:08 -0500

    Started refactoring unit tests.

--- Line.hpp
+++ Line.hpp
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-#ifndef __LINE_HPP__
-#define __LINE_HPP__
-
-#include "GuiElement.hpp"
-
-class Line : public GuiElement {
-	private:
-		vec2 start;
-        vec2 end;
-		vec3 color;
-
-    public:
-		Line();
-    
-        Line(vec2, vec2, vec3);
-		
-		~Line();
-		
-		void draw();
-		
-};
-
-=======
 #ifndef __LINE_HPP__
 #define __LINE_HPP__
 
@@ -53,5 +29,4 @@ class Line : public GuiElement {
 		void writeXml(std::ofstream& out) const;
 };
 
->>>>>>> 5c5799f (Finished XML parser. Refactoring Unit Tests.)
 #endif
