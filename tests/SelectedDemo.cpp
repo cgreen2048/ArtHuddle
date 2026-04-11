951447cd1193e2ec6445a3ce83ff58dd2bb1909d
Jacob Nkouka <jnkouka@nd.edu>
2026-04-11 03:22:09 -0400

    Attempt #2 at fixing the merge conflicts.

--- tests/SelectedDemo.cpp
+++ tests/SelectedDemo.cpp
@@ -34,7 +34,15 @@ int main() {
         return 1;
     }
 
-    Screen *screen = new Screen(X, Y);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << '\n';
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Screen *screen = new Screen(X, Y, renderer);
 
     int failure = selectedDemo(screen, window);
 
